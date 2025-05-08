ca77b0c8a478d2d928fe6ac47eddd997498b7d1f
Bibek Ray <bibekray.dec@gmail.com>
2025-05-09 01:04:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const data = {
-    AI Explained: [
+    "AI Explained": [
       { title: "Artificial Neural Network (ANN)", desc: "How does a machine find patterns in chaos?", img: "img1.png", url: "posts/ann.html" },
       { title: "Convolutional Neural Networks (CNN)", desc: "They technically destroy the image to see it properly.", img: "img2.png", url: "posts/cnn.html" },
       { title: "Long Short‑Term Memory (LSTM)", desc: "How we teach computers to be nostalgic.", img: "img3.png", url: "posts/lstm.html" }
@@ -7,7 +7,7 @@ const data = {
     Personal: [
       { title: "A Short Story on Generative Adversarial Networks (GANs)", desc: "See how two models try to fool each other.", img: "img4.png", url: "posts/gan.html" }
     ],
-    Marvel and other movies: [
+    "Marvel & Movies": [
       { title: "Fantastic 4 - What I expect", desc: "Are the Fantastic 4 really coming to Earth-616?", img: "img6.png", url: "posts/fantastic4.html" }
     ]
   };
